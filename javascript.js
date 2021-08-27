ef6bb12097460f6c77f2cfbb7ded99cbc5150739
Marta <martaguitart938@gmail.com>
2021-08-27 09:53:09 +0200

    M

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-welcome to the jungle!
+
+/*contador 
+/*ventanaalert
+
+HTML
+<button onclick="document.getElementById('demo').innerHTML = Date()">The time is?</button>
+*/
+
+/*<button onclick="alert('Hello')">Click me.</button>*/
+
+function
