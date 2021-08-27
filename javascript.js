06bbbf6cbef5cc2bb3adf2fe15f230b666fcdd03
timfel20 <timmyfelicia@gmail.com>
2021-08-27 08:59:36 +0200

    hh

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
-welcome to the jungle!
+const navSlide = function(){
+    const burger= document.querySelector ('.burger');
+    const container2=document.querySelector('.container2');
+    const back =document.querySelector('.back');
+    const container1 =document.querySelector('.container1');
+
+
+    burger.addEventListener('click', function(){
+    container2.classList.add('active');
+  
+
+
+    });
+
+    back.addEventListener('click', function(){
+    container2.classList.remove('active');
+    
+
+    });
+}
+navSlide();
