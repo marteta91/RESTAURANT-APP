56fa1409a0ae5cba2f6e6623493d38d31dfdc94f
timfel20 <timmyfelicia@gmail.com>
2021-08-30 12:02:11 +0200

    Df

--- javascript.js
+++ javascript.js
@@ -1,25 +1,5 @@
 
-
-const navSlide = function(){
-    const burger= document.querySelector ('.burger');
-    const container2=document.querySelector('.container2');
-    const back =document.querySelector('.back');
-    const container1 =document.querySelector('.container1');
-
-
-    burger.addEventListener('click', function(){
-    container2.classList.add('active');
-    container1.classList.add('hide');
-    
-  
-
-
-    });
-
-    back.addEventListener('click', function(){
-    container2.classList.remove('active');
-    
-
-    });
+function makeVisible(){
+    const container3 = document.querySelector('.container3')
+    container3.classList.add('toggle');
 }
-navSlide();
